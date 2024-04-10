138f5bd4c23200466820526c718a233c0b15891b
Eve Harrison <eveharrisonx@gmail.com>
2024-04-10 07:32:34 -0400

    Angular ONLY

--- Fidzulu_project_frontend/FidzuluApp/src/app/main-page/main-page.component.ts
+++ Fidzulu_project_frontend/FidzuluApp/src/app/main-page/main-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { MainService } from '../services/main.service';
 import { Bike } from '../models/bike';
 import { Book } from '../models/book';
 import { Dvd } from '../models/dvd';
 import { Food } from '../models/food';
 import { Laptop } from '../models/laptop';
 import { Toy } from '../models/toy';
+import { MainService } from '../services/main.service';
  
 @Component({
   selector: 'app-main-page',
@@ -24,11 +24,43 @@ export class MainPageComponent {
  
     errorMessage: string = "";
  
-    constructor(private mainService: MainService) { }
+    constructor(private mainService : MainService) { }
  
     ngOnInit() {
         this.getItems();
     }
+
+    showBikes: boolean = false;
+  showBooks: boolean = false;
+  showDVDs: boolean = false;
+  showFood: boolean = false;
+  showLaptops: boolean = false;
+  showToys: boolean = false;
+
+  toggleTable(table: string): void {
+    switch (table) {
+      case 'bikes':
+        this.showBikes = !this.showBikes;
+        break;
+      case 'books':
+        this.showBooks = !this.showBooks;
+        break;
+      case 'dvds':
+        this.showDVDs = !this.showDVDs;
+        break;
+      case 'food':
+        this.showFood = !this.showFood;
+        break;
+      case 'laptops':
+        this.showLaptops = !this.showLaptops;
+        break;
+      case 'toys':
+        this.showToys = !this.showToys;
+        break;
+      default:
+        break;
+    }
+  }
 
     getItems() {
         this.mainService.getBikes()
