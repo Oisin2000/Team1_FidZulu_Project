42f7561c4d4f4e604236c3ddf49d33c4af522ace
Eve Harrison <eveharrisonx@gmail.com>
2024-04-10 12:18:56 -0400

    Front end tests be testing

--- Fidzulu_project_frontend/FidzuluApp/src/app/about-page/about-page.component.spec.ts
+++ Fidzulu_project_frontend/FidzuluApp/src/app/about-page/about-page.component.spec.ts
@@ -1,21 +1,65 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AboutPageComponent } from './about-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { TeamServiceService } from '../services/team-service.service';
 
 describe('AboutPageComponent', () => {
   let component: AboutPageComponent;
   let fixture: ComponentFixture<AboutPageComponent>;
+  let teamService: TeamServiceService;
+  let getTeamDataSpy: jasmine.Spy;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AboutPageComponent]
+      declarations: [AboutPageComponent],
+      imports: [HttpClientModule],
+      providers: [TeamServiceService]
     });
     fixture = TestBed.createComponent(AboutPageComponent);
     component = fixture.componentInstance;
+    teamService = TestBed.inject(TeamServiceService);
+
+    getTeamDataSpy = spyOn(teamService, 'getTeamData').and.callThrough();
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
+
+  it('should display the correct button text', () => {
+    const element = fixture.nativeElement;
+    const button = element.querySelector('.team-button');
+  
+    expect(button.textContent).toContain('About Our Team');
+  });
+
+  it('should display the correct h1 content', () => {
+    const element = fixture.nativeElement;
+    const h1 = element.querySelector('.project-title');
+  
+    expect(h1.textContent).toContain('Team One Fidzulu Shop Front');
+  });
+
+  it('should display the correct h2 content', () => {
+    const element = fixture.nativeElement;
+    const h2 = element.querySelector('.project-sub-title');
+  
+    expect(h2.textContent).toContain('Click below to shop!');
+  });
+  
+  it('should toggle team data', () => {
+    component.toggleTeamData();
+    expect(component.showTeamData).toBe(true);
+    component.toggleTeamData();
+    expect(component.showTeamData).toBe(false);
+  });
+
+  it('should fetch team data on init', () => {
+    expect(getTeamDataSpy).toHaveBeenCalled();
+  });
+
+})
+
--- Fidzulu_project_frontend/FidzuluApp/src/app/services/team-service.service.spec.ts
+++ Fidzulu_project_frontend/FidzuluApp/src/app/services/team-service.service.spec.ts
@@ -1,16 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 
 import { TeamServiceService } from './team-service.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Team } from '../models/team';
 
-describe('TeamServiceService', () => {
+describe('TeamService', () => {
   let service: TeamServiceService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [TeamServiceService]
+    });
     service = TestBed.inject(TeamServiceService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch team data from the API via GET', () => {
+    const mockTeamData = {
+      team: 'Team One',
+      membersNames: ['Eve', 'Sean', 'Oisin', 'Liam', 'Robert']
+    };
+
+    service.getTeamData().subscribe(data => {
+      expect(data).toEqual(jasmine.objectContaining(mockTeamData));
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:3030/fidzulu/team');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockTeamData);
   });
 });
